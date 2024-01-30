38c5ded7d6f0ab7bface50b639ba0dd1e5aaf672
timofurs <timofurs@gmail.com>
2024-01-31 00:05:48 +0300

    Добавили Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 4;
+int b = 398;
+int c = 8;
+int d = 69;
+int e = 7;
+
+int max =a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
